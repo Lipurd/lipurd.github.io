b577ed13e6fed24797b921e172c42cc1ba1577b0
Dustin Kröger <me@staubrein.com>
2021-05-08 13:31:09 +0200

    add tailwind

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+  './_includes/**/*.html',
+  './_layouts/**/*.html',
+  './_posts/*.md',
+  './*.html',
+  './_site/**/*.html',
+],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
